ec55251c93e43e3e1254f19cc5bb824b01ebac22
Rafifoulol <72602496+Rafifoulol@users.noreply.github.com>
2022-09-29 19:58:31 +0200

    Fraction.toString

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,15 @@
 public class Fraction {
+
+    int nominateur;
+    int denominateur;
+
+    public Fraction(int nominateur, int denominateur) {
+        this.nominateur = nominateur;
+        this.denominateur = denominateur;
+    }
+
+    @Override
+    public String toString() {
+        return "Je suis une fraction.";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
+
 public class Main {
+
+    public static void main(String[] args){
+        Fraction f = new Fraction(1,2);
+
+        System.out.println(f.toString());
+    }
 }
